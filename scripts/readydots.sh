84ead1ea4d027620f688a7c153f938c7b4a08482
t60r <t60r.t60r@gmail.com>
2018-05-17 00:08:36 -0500

    adding a file

---
+++ scripts/readydots.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cp ~/.Xresources ~/git
+cp ~/.config/bspwm/bspwmrc ~/git/dots/.config/bspwm/
+cp ~/.config/sxhkd/sxhkdrc ~/git/dots/.config/sxhkd/
+cp ~/.config/polybar/config ~/git/dots/.config/polybar/
+cp ~/.config/neofetch/config.conf ~/git/dots/.config/neofetch/
+cp ~/.vimrc ~/git/dots/.vimrc
+cp ~/.xrdb/colors/* ~/git/dots/.xrdb/colors/
+cp ~/.xrdb/fonts/* ~/git/dots/.xrdb/fonts/
+cp ~/.xrdb/rofi/* ~/git/dots/.xrdb/rofi/
+cp ~/.xinitrc ~/git/dots/
+cp ~/.ncmpcpp/config ~/git/dots/.ncmpcpp/
+cp ~/builds/2bwm/2bwm/config.h ~/git/dots/2bwm
+cp ~/bin/colors.sh ~/git/dots/scripts
+cp ~/.config/libinput-gestures.conf ~/git/dots/.config
+cp ~/bin/readydots.sh ~/git/dots/scripts
+
+#END
